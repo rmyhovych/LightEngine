491529b40d66336c2380bd2f4469469befa8d980
Ross Myhovych <rmyhovych@gmail.com>
2018-10-30 13:35:50 -0400

    adding glew

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -1,19 +1,41 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <stdio.h>
 
 int main()
 {
+	//	-- START GLFW --
 	glfwInit();
 
+	//	Version 3.2
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+
+	//	Core profile OpenGL
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//	Forward compatibility
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
+	//	Resizable window
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
+	//	Window creation + OpenGL Context
 	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", nullptr, nullptr);
 	glfwMakeContextCurrent(window);
 
+	//	Setup GLEW
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	//	Code
+	GLuint vertexBuffer;
+	glGenBuffers(1, &vertexBuffer);
+
+	printf("%u\n", vertexBuffer);
+
+	//	Event Loop
 	while (!glfwWindowShouldClose(window))
 	{
 		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
@@ -24,6 +46,7 @@ int main()
 		glfwPollEvents();
 	}
 
+	//	-- END GLFW --
 	glfwTerminate();
 	return 0;
 }
