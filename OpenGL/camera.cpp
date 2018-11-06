bdda5386399b02d9eaa418d84d0229d645e621f7
Ross Myhovych <rmyhovych@gmail.com>
2018-11-05 21:14:43 -0500

    creating camera constructor

--- OpenGL/camera.cpp
+++ OpenGL/camera.cpp
@@ -1,8 +1,24 @@
 #include "camera.h"
 
+Camera::Camera(bool* input, glm::vec3 position, glm::vec3 focus, float zoom) :
+	input_(input),
+	position_(position),
+	focus_(focus),
+	zoom_(zoom),
+	up_(glm::vec3(0, 1, 0)),
 
-
-Camera::Camera()
+	angleH_(0),
+	angleV_(0)
 {
+	view_ = glm::lookAt(zoom * position_, focus_, up_);
 }
 
+float* Camera::passAngles()
+{
+	return nullptr;
+}
+
+void Camera::move()
+{
+	
+}
--- OpenGL/camera.h
+++ OpenGL/camera.h
@@ -4,11 +4,24 @@
 class Camera
 {
 public:
-	Camera();
+	Camera(bool* input, glm::vec3 position = glm::vec3(0, 0, 1), glm::vec3 focus = glm::vec3(0, 0, 0), float zoom = 1);
+
+	float* passAngles();
+
+	void move();
 
 private:
-	glm::vec3 position;
-	glm::vec3 focus;
-	float zoom;
+	glm::vec3 position_;
+	glm::vec3 focus_;
+	glm::vec3 up_;
+
+	float zoom_;
+
+	glm::mat4 view_;
+
+	float angleH_;
+	float angleV_;
+
+	bool* input_;
 };
 
