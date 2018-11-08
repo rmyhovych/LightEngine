2bc4da2b8d1b593a96e4c296a1d4056f6b0a3001
Ross Myhovych <rmyhovych@gmail.com>
2018-11-08 12:10:58 -0500

    adding light color specular reflexion

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -26,9 +26,9 @@ int main()
 
 	std::vector<glm::vec3> positions = {
 		glm::vec3(0.6f,  0.2f,  -1.2f),
-		glm::vec3(0.0f,  0.0f, -15.0f),
+		glm::vec3(0.0f,  0.0f, -7.0f),
 		glm::vec3(-1.5f, -2.2f, -2.5f),
-		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(-3.8f, -2.0f, -6.3f),
 		glm::vec3(2.4f, -0.4f, -3.5f),
 		glm::vec3(-1.7f,  3.0f, -7.5f),
 		glm::vec3(1.3f, -2.0f, -2.5f),
