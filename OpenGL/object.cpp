598710864ec4d4180cafeea42e067cc6e3a7585f
Ross Myhovych <rmyhovych@gmail.com>
2018-11-21 09:39:07 -0500

    adding linking functions to object

--- OpenGL/object.cpp
+++ OpenGL/object.cpp
@@ -2,12 +2,23 @@
 
 
 
-Object::Object()
-{
+Object::Object(glm::vec3& position, glm::vec3& scale, glm::vec3& color) :
+	position_(position),
+	scale_(scale),
+	color_(color),
 
+	rotation_(glm::mat3(1)),
+	model_(glm::mat4(1)),
+
+	modified_(0xff)
+{
 }
 
-
-Object::~Object()
+void Object::update()
 {
+	if (modified_ & 0b1 == 0b1)
+	{
+
+	}
 }
+
--- OpenGL/object.h
+++ OpenGL/object.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "window_gl.h"
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtc/type_ptr.hpp"
@@ -7,17 +8,37 @@
 class Object
 {
 public:
-	Object();
-	~Object();
+	Object(glm::vec3& position, glm::vec3& scale, glm::vec3& color);
+
+	void move(glm::vec3& vector);
+	void rotate(glm::vec3& axis, float angle);
+	void scale(glm::vec3& vector);
+
+	void linkRotation(const char* name, GLuint ID);
+	void linkModel(const char* name, GLuint ID);
+	void linkColor(const char* name, GLuint ID);
+
+	void use();
+
+private:
+	void update();
 
 protected:
+
+	// Object properties
 	glm::vec3 position_;
 	glm::vec3 scale_;
 	
-	glm::mat4 rotation_;
+	glm::mat3 rotation_;
 	glm::mat4 model_;
 
 	glm::vec3 color_;
 
+	char modified_;
+
+	// Shader uniforms
+	GLuint rotationPtr_;
+	GLuint modelPtr_;
+	GLuint colorPtr_;
 };
 
