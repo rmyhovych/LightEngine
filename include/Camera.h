b2a6d876ee6cad3a6723f7396c05e060994aa523
Ross Myhovych <rmyhovych@gmail.com>
2019-04-18 23:43:48 -0400

    separating all the files from the vs project

---
+++ include/Camera.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "glm.hpp"
+#include "gtc/matrix_transform.hpp"
+#include "gtc/type_ptr.hpp"
+
+#include <iostream>
+
+#include <cmath>
+
+#define PI 3.14159265359f
+
+class Camera
+{
+public:
+	Camera(int width, int height, float zoom = 20, 
+		float angleH = 0, float angleV = PI / 2,
+		const glm::vec3& focus = glm::vec3(0, 0, 0));
+	
+
+	void resize(int width, int height);
+	
+	glm::mat4& getPV();
+	
+	void rotate(float x, float y);
+	void move(float x, float y, float z);
+
+
+private:
+	void update();
+
+private:
+
+	float zoom;
+
+	glm::vec3 focus;
+	glm::vec3 direction;
+	glm::vec3 position;
+	glm::vec3 up;
+
+	glm::mat4 projection;
+	glm::mat4 view;
+	glm::mat4 pv;
+
+
+	float angleH;
+	float angleV;
+};
---
+++ include/Object.h
@@ -0,0 +1,82 @@
+#pragma once
+
+#include "Shader.h"
+#include "UniformHandling.h"
+
+#include "glm.hpp"
+#include "gtc/matrix_transform.hpp"
+#include "gtc/type_ptr.hpp"
+
+
+
+class Object
+{
+public:
+
+	Object(
+		const glm::vec3& position = glm::vec3(0), 
+		const glm::vec3& orientation = glm::vec3(0),
+		const glm::vec3& scale = glm::vec3(1),
+		const glm::vec3& color = glm::vec3(1));
+
+
+
+
+	void addPosition(const glm::vec3& position);
+
+	void setPosition(const glm::vec3& position);
+
+
+
+	void addOrientation(const glm::vec3& rotation);
+
+	void setOrientation(const glm::vec3& rotation);
+
+
+
+
+	void addScale(const glm::vec3& v);
+
+	void setScale(const glm::vec3& v);
+
+	void setScale(float v);
+
+
+
+	void setColor(const glm::vec3& color);
+
+	void setColor(float r, float g, float b);
+
+
+	void useDepth();
+
+	void use(ObjectUniforms& uniforms);
+
+private:
+
+	void model();
+
+
+	static glm::vec3 right;
+	static glm::vec3 top;
+	static glm::vec3 back;
+
+
+	glm::mat4 mScale;
+	glm::mat4 mRotation;
+	glm::mat4 mTranslation;
+
+	glm::mat4 mModel;
+
+
+	glm::vec3 color;
+
+
+};
+
+
+
+
+
+
+
