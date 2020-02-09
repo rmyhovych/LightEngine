d9d592fc73439b441ef110e9cad27f05db19e793
ross <rmyhovych@gmail.com>
2020-02-08 20:37:44 -0500

    tweaking structure again
    Signed-off-by: ross <rmyhovych@gmail.com>

---
+++ PicoEngine/main.cpp
@@ -0,0 +1,8 @@
+#import "File/FileData.h"
+
+int main()
+{
+	FileData* fd = new FileData();
+
+	return 0;
+}
--- PicoFramework/src/ObjectManager/Object/ObjectRenderer.h
+++ PicoFramework/src/ObjectManager/Object/ObjectRenderer.h
@@ -3,9 +3,9 @@
 #include "Graphics/Shader.h"
 #include "Graphics/UniformHandling.h"
 
-#include "glm.hpp"
-#include "gtc/matrix_transform.hpp"
-#include "gtc/type_ptr.hpp"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
 
 
 class ObjectRenderer
